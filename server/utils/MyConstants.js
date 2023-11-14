b7db3eeced16d785019a0bb500431db0bbeb9ec4
quanglong310 <quanglong310@gmail.com>
2023-11-14 13:56:46 +0700

    init

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+    DB_SERVER: 'cluster0.pizxpsq.mongodb.net',
+    DB_USER: 'quanglong310',
+    DB_PASS: '3102003lonGg',
+    DB_DATABASE: 'shoppingonline',
+    JWT_SECRET: 'quanglongcaigicungduoc',
+    JWT_EXPIRES: '36000000', // in milliseconds
+    EMAIL_USER: 'quanglongdateh@gmail.com', // gmail service
+    EMAIL_PASS: 'anykdrkjyuxbozwi'
+  };
+  module.exports = MyConstants;
